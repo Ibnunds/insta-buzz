14a96b81e3e8c77dc205afbea7fa0698e47b3d67
ibnuputraard <ibnu.putra@nawadata.id>
2024-02-05 20:09:32 +0700

    update stealth mode

--- bin/browser.js
+++ bin/browser.js
@@ -1,10 +1,18 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require("puppeteer-extra");
+
+// Add stealth plugin and use defaults
+const pluginStealth = require("puppeteer-extra-plugin-stealth");
+const { executablePath } = require("puppeteer");
+
+// Use stealth
+puppeteer.use(pluginStealth());
 
 // Initiate the browser
 const openBrowser = async () => {
   const browser = await puppeteer.launch({
     args: ["--lang=en-US,en", "--no-sandbox", "--disable-setuid-sandbox"],
-    headless: "new",
+    headless: /*"new"*/ false,
+    executablePath: executablePath(),
   });
 
   return browser;
